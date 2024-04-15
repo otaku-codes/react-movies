bed2c2bea80b104ebca94292f0e8d13507b21134
otaku-codes <himanshupapola.ph@gmail.com>
2024-04-15 20:29:29 +0530

    css fix

--- src/App.js
+++ src/App.js
@@ -106,13 +106,13 @@ function Movies({ movies, title, setCurrentSelection }) {
         {title === "Trending" ? (
           <div className="trend-btn-grp">
             <button
-              className="trend-btn"
+              className="trend-btns"
               onClick={() => setCurrentSelection("movie")}
             >
               <i className="fa fa-play-circle mr-2">&nbsp; </i>Movies
             </button>
             <button
-              className="trend-btn"
+              className="trend-btns"
               onClick={() => setCurrentSelection("tv")}
             >
               <i className="fa fa-list mr-2"></i>&nbsp; TV Shows
